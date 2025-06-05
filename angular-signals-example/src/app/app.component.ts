d8fd96bfb2d60470139709aac8e72cd7fa221838
Hoang Minh Tien <tinhoang7901@gmail.com>
2025-06-05 21:58:51 +0700

    real world use case fo computed signal

--- angular-signals-example/src/app/app.component.ts
+++ angular-signals-example/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal, effect, computed } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,6 +19,18 @@ export class AppComponent {
   quantity = signal(20);
   totalPrice = computed(() => this.price * this.quantity());
 
+  products = signal([
+    { id: 1, name: 'Milk', price: 10.33 },
+    { id: 2, name: 'Bread', price: 4.65 },
+    { id: 3, name: 'Cream', price: 6.7 }
+  ]);
+
+  filterName = signal('');
+
+  filteredProducts = computed(() => {
+    const filter = this.filterName().toLowerCase();
+    return this.products().filter(product => product.name.toLowerCase().includes(filter));
+  });
   
   constructor() {
     effect(() => {
@@ -33,6 +46,11 @@ export class AppComponent {
   changeQuantity(event: Event) {
     const value = (event.target as HTMLInputElement).valueAsNumber;
     this.quantity.set(value);
+  }
+
+  filterProducts(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.filterName.set(value);
   }
 
 }
